c3a882c8f03d57ebd96bd9a42e184f6cb4970138
tanghongyu <3266415926@qq.com>
2020-06-12 13:12:29 +0800

    stores

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App'
 import Api from './api/index'
+import store from './stores'
 
 Vue.config.productionTip = false
 
@@ -8,6 +9,7 @@ App.mpType = 'app'
 let api = new Api();
 Vue.prototype.$api = api;
 const app = new Vue({
+	store,
     ...App
 })
 app.$mount()
--- pages/service/index.vue
+++ pages/service/index.vue
@@ -2,9 +2,7 @@
 	<view>
 		<view class="yellow">
 			111
-			<view class="aa">
-				222
-			</view>
+			<button type="default" @click="weixin">登陆</button>
 		</view>
 	</view>
 </template>
@@ -17,7 +15,35 @@
 			}
 		},
 		methods: {
-			
+			getImage(){
+				uni.chooseImage({
+				    count: 6, //默认9
+				    sizeType: ['original', 'compressed'], //可以指定是原图还是压缩图，默认二者都有
+				    sourceType: ['album'], //从相册选择
+				    success: function (res) {
+						console.log(res)
+				        console.log(JSON.stringify(res.tempFilePaths));
+				    }
+				});
+			},
+			weixin(){
+				uni.request({
+					url:'https://api.weixin.qq.com/cgi-bin/token',
+					method:'GET',
+					data:{
+						grant_type: 'client_credential',
+						appid: 'wx8c312393c7435d11',
+						secret: '2989c23024099d7593dc4a29c4cb083a'
+					},
+					header:{},
+					success:(res)=>{
+						console.log(res)
+					},
+					fail:(error)=>{
+						
+					}
+				})
+			}
 		}
 	}
 </script>
